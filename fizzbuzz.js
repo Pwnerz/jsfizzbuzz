5d156ad4f4e5f0337b4cfadfb0daf3eb4d9b6ca9
Alex <supamumas@gmail.com>
2021-03-05 10:39:05 +0100

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,12 @@
 *Fizzbuzz Solution
 
 for (var i = 1; i <= 100; i++) {
+  //Note the importance of the order.
 if (i % 3 === 0 && i % 5 === 0) {
 console.log("fizzbuzz");}
 else if (i % 3 === 0) {
 console.log("fizz");}
-else if (i%5 === 0) {
+else if (i % 5 === 0) {
 console.log("buzz");}
 else {
 console.log(i);}
